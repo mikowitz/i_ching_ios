1419719b3c189ad6a9654e85f7c267bbbf2647d6
Michael Berkowitz <michael.berkowitz@gmail.com>
2015-09-07 14:00:46 -0400

    Remove comments

--- app/views/hexagram_view.rb
+++ app/views/hexagram_view.rb
@@ -8,12 +8,5 @@ class HexagramView < UIView
         st.frame = { t: y, h: dim * 2, w: self.frame.size.width - dim * 2, l: dim }
       end
     end
-    # [1, 3, 5, 7, 9, 11].map { |i| i * size }.each do |y|
-    #   rmq(self).append(UIView, :hexagram_line).style do |st|
-    #     st.frame = { t: y, h: size, w: size * 11, l: size }
-    #   end
-    # end
-    # dimension = size * 13
-    # rmq(self).style { |st| st.frame = { fr: 15, w: dimension, h: dimension } }
   end
 end
